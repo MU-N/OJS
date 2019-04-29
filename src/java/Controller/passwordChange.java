35ec376820564f0a1865d06eff5076613a7ce21c
nasser <mohamedn.elsayed01090@gmail.com>
2019-04-29 19:32:34 +0200

    hi hi captin

---
+++ src/java/Controller/passwordChange.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import DB.UserDOA;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.User;
+
+/**
+ *
+ * @author moham
+ */
+public class passwordChange extends HttpServlet {
+        User u = new User();
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            u = (User) request.getSession().getAttribute("user");
+            
+            String cp= request.getParameter("currentPass");
+            String np=request.getParameter("newPass");
+            String rnp=request.getParameter("reNewPass");
+            String pass = u.getPass();
+            String uname = u.getName();
+            if (pass.equals(cp)&&np.equals(rnp)){
+            System.out.println(" N "+u.getUname()+" cp "+cp + " np "+np +" rnp "+ rnp );
+            if(new UserDOA().update_password(np,uname))
+            {
+                u.setPass(np);
+                RequestDispatcher dispatcher = request.getRequestDispatcher("/profile.jsp");
+                dispatcher.forward(request, response);
+            }
+            }
+            else 
+            {
+                System.out.println(" problem  ");
+            }
+
+        }
+    
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
